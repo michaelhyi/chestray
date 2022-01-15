f7c9993b79478183bb95b8f53e309f1a9a8b065e
23yimichael <23yimichael@gmail.com>
2022-01-14 23:44:58 -0800

    worked on image processing

--- src/screens/results.js
+++ src/screens/results.js
@@ -1,9 +1,35 @@
-import { StyleSheet, View, Text } from "react-native";
+import { useState, useContext, useEffect } from "react";
+import { StyleSheet, View, Text, ActivityIndicator, Image } from "react-native";
+
+import { process } from "../functions/tf.js";
+
+import Context from "../utils/context.js";
 
 export default function Results() {
+  const { image } = useContext(Context);
+  const [processing, setProcessing] = useState(true);
+  const [diagnosis, setDiagnosis] = useState(undefined);
+
+  useEffect(() => {
+    process(image, setDiagnosis, setProcessing);
+  }, []);
+
+  useEffect(() => {
+    if (diagnosis) {
+    }
+  }, [diagnosis]);
+
+  if (processing) {
+    <View style={styles.container}>
+      <Text>Processing...</Text>
+      <ActivityIndicator />
+    </View>;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Results Screen</Text>
+      <Image source={{ uri: image.uri }} />
+      <Text>{diagnosis}</Text>
     </View>
   );
 }
