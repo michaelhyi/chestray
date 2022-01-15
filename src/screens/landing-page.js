164ba289e97b72c7a6159b9568e00c758c892561
Jason Chen <californiaroad1@gmail.com>
2022-01-14 20:04:33 -0800

    started working on login page and landing page

--- src/screens/landing-page.js
+++ src/screens/landing-page.js
@@ -1,9 +1,48 @@
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
 
-export default function LandingPage() {
+export default function LandingPage({navigation}) {
   return (
     <View style={styles.container}>
-      <Text>Landing Page</Text>
+      <View style={styles.topText1Container}>
+        <Text style={styles.topText1}>Select</Text>
+        <Text style={styles.topText2}>Login</Text>
+        <View style={styles.line}/>
+      </View>
+      <View style={styles.cards}>
+          <View style={styles.rectangle}>
+            <View>
+              <Text style={styles.largeText}>Doctor</Text>
+              <Text style={styles.smallText}>Upload and Manage Diagonsis</Text>
+              <TouchableOpacity onPress={() => navigation.navigate("Login")}>
+                <View style={styles.button}>
+                  <Text style={styles.buttonText}>Log In</Text>
+                </View>
+              </TouchableOpacity>
+            </View>
+          </View>
+          <View style={styles.rectangle1}>
+            <View>
+              <Text style={styles.largeText}>Patient</Text>
+              <Text style={styles.smallText}>View your Diagonsis</Text>
+              <TouchableOpacity>
+                <View style={styles.button}>
+                  <Text style={styles.buttonText}>Select</Text>
+                </View>
+              </TouchableOpacity>
+            </View>
+          </View>
+          <View style={styles.rectangle1}>
+            <View>
+              <Text style={styles.largeText}>Admin</Text>
+              <Text style={styles.smallText}>Monitor Server Status</Text>
+              <TouchableOpacity>
+                <View style={styles.button}>
+                  <Text style={styles.buttonText}>Log In</Text>
+                </View>
+              </TouchableOpacity>
+            </View>
+          </View>
+      </View>
     </View>
   );
 }
@@ -11,7 +50,92 @@ export default function LandingPage() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    margin: 50,
   },
+  topText1Container: {
+    marginTop: 20,
+  },
+  topText1: {
+    fontFamily: "Avenir-Light",
+    fontSize: 45,
+  },
+  topText2: {
+    fontFamily: "Avenir-Heavy",
+    fontSize: 45,
+    color: "#091d36"
+  },
+  line: {
+    marginTop: 15,
+    backgroundColor: "#3a4e7a",
+    height: 3,
+    width: "50%",
+    borderRadius: 10,
+  },
+
+  rectangle: {
+    backgroundColor: "white",
+    flexDirection: "row",
+    marginTop: 30,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 3.84,
+    elevation: 10,
+    marginBottom: 10,
+    borderRadius: 15,
+  },
+
+  rectangle1: {
+    backgroundColor: "white",
+    flexDirection: "row",
+    marginTop: 15,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 3.84,
+    elevation: 10,
+    marginBottom: 10,
+    borderRadius: 15,
+  },
+
+  largeText: {
+    marginTop: 15,
+    fontFamily: "Avenir-Heavy",
+    fontSize: 35,
+    marginLeft: 25,
+    color: "#3a4e7a"
+  },
+
+  smallText: {
+    marginLeft: 25,
+    fontFamily: "Avenir-Light",
+    marginTop: 4,
+    color: "#8D8D8D",
+    fontSize: 16,
+  },
+
+  button: {
+    marginLeft: 25,
+    fontFamily: "Avenir-Light",
+    marginTop: 10,
+    marginBottom: 18,
+    backgroundColor: "#3a4e7a",
+    fontSize: 16,
+    width: 90,
+    borderRadius: 10000,
+  },
+  buttonText: {
+    alignContent: "center",
+    textAlign: "center",
+    margin: 5,
+    fontFamily: "Avenir-Light",
+    color: "white",
+    fontSize: 15,
+  }
 });
--- src/screens/login.js
+++ src/screens/login.js
@@ -1,9 +1,35 @@
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, TouchableOpacity, Dimensions, TextInput } from "react-native";
 
 export default function Login() {
   return (
     <View style={styles.container}>
-      <Text>Login Page</Text>
+      <View style={styles.topText1Container}>
+        <Text style={styles.topText1}>Enter</Text>
+        <Text style={styles.topText2}>Information</Text>
+        <View style={styles.line}/>
+      </View>
+      <View style={styles.cards}>
+          <View style={styles.rectangle}>
+            <View>
+              <Text style={styles.smallText}>Username</Text>
+              <View style={styles.textInputContainer}>
+                <TextInput style={styles.input}/>
+              </View>
+              <Text style={styles.smallText}>Password</Text>
+              <View style={styles.textInputContainer1}>
+                <TextInput style={styles.input}/>
+              </View>
+              <View style={styles.buttonContainer}>
+              <TouchableOpacity>
+                <View style={styles.button}>
+                  <Text style={styles.buttonText}>Log In</Text>
+                </View>
+              </TouchableOpacity>
+          </View>
+            </View>
+          </View>
+          
+      </View>
     </View>
   );
 }
@@ -11,7 +37,88 @@ export default function Login() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    margin: 50,
   },
+  topText1Container: {
+    marginTop: 20,
+  },
+  topText1: {
+    fontFamily: "Avenir-Light",
+    fontSize: 45,
+  },
+  topText2: {
+    fontFamily: "Avenir-Heavy",
+    fontSize: 45,
+    color: "#091d36"
+  },
+  line: {
+    marginTop: 15,
+    backgroundColor: "#3a4e7a",
+    height: 3,
+    width: "50%",
+    borderRadius: 10,
+  },
+  rectangle: {
+    backgroundColor: "white",
+    flexDirection: "row",
+    marginTop: Dimensions.get('window').height/2 - 275,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 3.84,
+    elevation: 10,
+    marginBottom: 10,
+    borderRadius: 15,
+  },
+
+  smallText: {
+    marginLeft: 25,
+    fontFamily: "Avenir-Light",
+    marginTop: 20,
+    color: "#8D8D8D",
+    fontSize: 16,
+  },
+  input: {
+    height: 40,
+    backgroundColor: "#c2d2e9",
+    width: 237,
+    padding: 10,
+    borderRadius: 15,
+  },
+  textInputContainer: {
+    marginLeft: 25,
+    marginTop: 5,
+    borderRadius: 20,
+  },
+  textInputContainer1: {
+    marginLeft: 25,
+    marginTop: 5,
+    borderRadius: 20,
+    marginBottom: 5,
+  },
+  button: {
+    marginLeft: 25,
+    fontFamily: "Avenir-Light",
+    marginTop: 10,
+    marginBottom: 18,
+    backgroundColor: "#3a4e7a",
+    fontSize: 16,
+    width: 90,
+    borderRadius: 10000,
+  },
+  buttonText: {
+    alignContent: "center",
+    textAlign: "center",
+    margin: 5,
+    fontFamily: "Avenir-Light",
+    color: "white",
+    fontSize: 15,
+  },
+  buttonContainer: {
+    alignContent: "center",
+    alignItems: "center",
+  }
 });
