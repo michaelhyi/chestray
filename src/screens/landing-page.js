b192bc45200eb28d8efba8f92f8951d1e4fc82f2
23yimichael <23yimichael@gmail.com>
2022-01-14 19:36:47 -0800

    worked on ui

--- src/screens/landing-page.js
+++ src/screens/landing-page.js
@@ -1,17 +1,87 @@
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
+import Constants from "expo-constants";
 
-export default function LandingPage() {
+import Button from "../components/button.js";
+
+export default function LandingPage({ navigation }) {
   return (
     <View style={styles.container}>
-      <Text>Landing Page</Text>
+      <View style={styles.header}>
+        <Text style={styles.lightText}>Select</Text>
+        <Text style={styles.boldText}>Login</Text>
+        <View style={styles.horizontalRule} />
+      </View>
+      <View style={styles.body}>
+        <View style={styles.box}>
+          <Text style={styles.boldText}>Doctor</Text>
+          <Text>Upload and Manage Diagnosis</Text>
+          <View style={{ flexDirection: "row" }}>
+            <Button text="Login" onPress={() => navigation.navigate("Login")} />
+            <Button
+              text="Sign Up"
+              onPress={() => navigation.navigate("Sign Up")}
+            />
+          </View>
+        </View>
+        <View style={styles.box}>
+          <Text style={styles.boldText}>Patient</Text>
+          <Text>View Your Diagnosis</Text>
+          <View style={{ flexDirection: "row" }}>
+            <Button
+              text="Enter Diagnosis ID"
+              onPress={() => navigation.navigate("Enter Diagnosis ID")}
+            />
+          </View>
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    paddingTop: Constants.statusBarHeight,
+  },
+
+  header: {
+    marginLeft: 35,
+    marginTop: 10,
+    flexDirection: "column",
+  },
+
+  lightText: {
+    fontSize: 36,
+    fontWeight: "normal",
+  },
+
+  boldText: {
+    fontSize: 36,
+    fontWeight: "bold",
+  },
+
+  horizontalRule: {
+    marginTop: 15,
+    borderBottomColor: "black",
+    borderBottomWidth: 2,
+    marginRight: 250,
+  },
+
+  body: {
+    flexDirection: "column",
+  },
+
+  box: {
+    backgroundColor: "white",
+    paddingVertical: 15,
+    paddingLeft: 20,
+    marginHorizontal: 35,
+    marginVertical: 35,
+    shadowOffset: {
+      width: 5,
+      height: 5,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 5,
+    borderRadius: 20,
   },
 });
--- src/screens/login.js
+++ src/screens/login.js
@@ -1,17 +1,79 @@
-import { StyleSheet, View, Text } from "react-native";
+import {
+  Dimensions,
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+} from "react-native";
+import Constants from "expo-constants";
 
-export default function Login() {
+import Button from "../components/button.js";
+import { googleSignIn } from "../functions/auth.js";
+
+export default function LandingPage({ navigation }) {
   return (
     <View style={styles.container}>
-      <Text>Login Page</Text>
+      <View style={styles.header}>
+        <Text style={styles.lightText}>Enter</Text>
+        <Text style={styles.boldText}>Information</Text>
+        <View style={styles.horizontalRule} />
+      </View>
+      <View style={styles.body}>
+        <View style={styles.box}></View>
+        <Button text="Sign in with Google" onPress={googleSignIn} />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    paddingTop: Constants.statusBarHeight,
     flex: 1,
+  },
+
+  header: {
+    marginLeft: 35,
+    marginTop: 10,
+    flexDirection: "column",
+  },
+
+  lightText: {
+    fontSize: 36,
+    fontWeight: "normal",
+  },
+
+  boldText: {
+    fontSize: 36,
+    fontWeight: "bold",
+  },
+
+  horizontalRule: {
+    marginTop: 15,
+    borderBottomColor: "black",
+    borderBottomWidth: 2,
+    marginRight: 250,
+  },
+
+  body: {
+    flexDirection: "column",
+    flexGrow: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+
+  box: {
+    backgroundColor: "white",
+    paddingVertical: 15,
+    paddingLeft: 20,
+    marginHorizontal: 35,
+    marginVertical: 35,
+    shadowOffset: {
+      width: 5,
+      height: 5,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 5,
+    borderRadius: 20,
   },
 });
